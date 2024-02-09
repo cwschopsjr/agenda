b570334bebf4c891ca118b1143986c93818a5a88
cwschopsjr <cwschops.jr@gmail.com>
2024-02-09 15:03:53 -0300

    Atualização dos campos da tabela

--- contact/admin.py
+++ contact/admin.py
@@ -4,14 +4,14 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'nome', 'sobrenome', 'telefone',
+    list_display = 'id', 'unidade', 'nome_do_paciente', 'quarto', 'id_leito', 'telefone'
     ordering = '-id',
     # list_filter = 'last_name',
-    search_fields = 'id', 'nome', 'sobrenome',
+    search_fields = 'id', 'nome_do_paciente', 'unidade', 'quarto', 'id_leito'
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = 'telefone',
-    list_display_links = 'id', 'nome',
+    list_editable = 'nome_do_paciente',
+    list_display_links = 'id', 'unidade',
 
 
 @admin.register(models.Category)
--- contact/models.py
+++ contact/models.py
@@ -47,16 +47,18 @@ class Contact(models.Model):
     class Meta:
         verbose_name = 'Leito'
 
-    nome = models.CharField(max_length=50)
-    sobrenome = models.CharField(max_length=50)
-    telefone = models.CharField(max_length=50)
+    unidade = models.ForeignKey(
+        Units, on_delete=models.SET_NULL, blank=True, null=True)
+    id_leito = models.CharField(max_length=10, blank=True, null=True)
+    quarto = models.CharField(max_length=10, blank=True, null=True)
+    tipo_de_leito = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, blank=True, null=True)
+    nome_do_paciente = models.CharField(max_length=50, blank=True, null=True)
+    diagnostico = models.CharField(max_length=50, blank=True, null=True)
+    telefone = models.CharField(max_length=50, blank=True, null=True)
     email = models.EmailField(max_length=254, blank=True)
     anotacoes = models.TextField(blank=True)
     show = models.BooleanField(default=True)
-    unidade = models.ForeignKey(
-        Units, on_delete=models.SET_NULL, blank=True, null=True)
-    tipo_de_leito = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, blank=True, null=True)
     uso_do_leito = models.ForeignKey(
         Category2, on_delete=models.SET_NULL, blank=True, null=True)
     enviar_arquivo = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
@@ -65,4 +67,4 @@ class Contact(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self) -> str:
-        return f'{self.nome} {self.sobrenome}'
+        return f'{self.unidade} {self.quarto} {self.id_leito} {self.nome_do_paciente}'
