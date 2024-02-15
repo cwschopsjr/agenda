feab3022fc0ab48091ab6e44b307fd4a985ab347
cwschopsjr <cwschops.jr@gmail.com>
2024-02-15 18:11:59 -0300

    Criação dos partials head e header e criação do menu

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -7,6 +7,7 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
     }
 
     return render(request, 'contact/index.html', context)
@@ -16,8 +17,11 @@ def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
+    contact_name = f'{single_contact.nome_do_paciente} - '
+
     context = {
         'contact': single_contact,
+        'site_title': contact_name
     }
 
     return render(request, 'contact/contact.html', context)
