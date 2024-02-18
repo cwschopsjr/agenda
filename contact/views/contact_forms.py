6dd5058928face12a9eda627f9d18c073c6b6a26
cwschopsjr <cwschops.jr@gmail.com>
2024-02-18 10:12:35 -0300

    Criação da view contact_forms, atualização do template create.html com o crsf_token

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,12 @@
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, render, redirect
+from django.db.models import Q
+from contact.models import Contact
+
+
+def create(request):
+    post = request.post
+
+    context = {}
+
+    return render(request, 'contact/create.html', context)
