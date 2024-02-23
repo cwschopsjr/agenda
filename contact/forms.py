643cbf824f806dbbe822124c25ac772c2eba49fd
cwschopsjr <cwschops.jr@gmail.com>
2024-02-23 10:23:14 -0300

    Criação do template register.html e configurações de mensagens

--- contact/forms.py
+++ contact/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from contact.models import Contact
 from django.shortcuts import render
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class ContactForm(forms.ModelForm):
@@ -31,3 +33,33 @@ class ContactForm(forms.ModelForm):
             )
             self.add_error('nome_do_paciente', msg)
         return super().clean()
+
+
+class RegisterForm(UserCreationForm):
+    primeiro_nome = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    sobrenome = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = (
+            'primeiro_nome', 'sobrenome', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe este e-mail', code='invalid')
+            )
+
+        return email
